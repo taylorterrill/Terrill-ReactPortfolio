59feb2995346dabaaf26a8549f15bf746b869027
PeachDoors <taylorpterrill@gmail.com>
2022-11-12 15:51:07 -0700

    setup files, some css styling, import google font, set up css variables

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+import Nav from './components/nav/Nav';
+import Portfolio from './components/portfolio/Portfolio';
+import Resume from './components/resume/Resume';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Portfolio />
+      <Resume />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default App
