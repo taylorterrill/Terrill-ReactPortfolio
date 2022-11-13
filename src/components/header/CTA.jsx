93a8793aa13812912147e3bf752c321b6a4c7da2
PeachDoors <taylorpterrill@gmail.com>
2022-11-13 15:35:49 -0700

    start header

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function CTA() {
+  return (
+    <div className='cta'>
+        <a href='#resume' className='btn'>Resume</a>
+        <a href='#contact' className='btn btn-primary'>Contact</a>
+    </div>
+  )
+}
+
+export default CTA
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import './header.css';
+import CTA from './CTA';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className='container header__container'>
+        <h5>Thanks for stopping by. My name is</h5>
+        <h1>Taylor Terrill</h1>
+        <h5 className='text-light'>Fullstack Developer</h5>
+        <CTA />
+      </div>
+    </header>
   )
 }
 
