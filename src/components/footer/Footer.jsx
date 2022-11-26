604ea298105bbea17f91222a26d4badbe7280bf8
PeachDoors <taylorpterrill@gmail.com>
2022-11-26 16:55:10 -0700

    footer done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,17 @@ import './footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer WIP</div>
+    <footer>
+      <a href='#' className='footer__title'>Taylor Terrill</a>
+      <ul className='permalinks'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#portfolio'>Portfolio</a></li>
+        <li><a href='#resume'>Resume</a></li>
+        <li><a href='#contact'>Contact</a></li>
+      </ul>
+
+    </footer>
   )
 }
 
